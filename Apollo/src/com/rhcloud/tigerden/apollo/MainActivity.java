b2a8e87492cf11392458d7069ad73372c3bf98fd
clemsonwaterski <clemsonwaterski@gmail.com>
2014-06-04 17:31:10 -0400

    Completed next tutorial

--- Apollo/src/com/rhcloud/tigerden/apollo/MainActivity.java
+++ Apollo/src/com/rhcloud/tigerden/apollo/MainActivity.java
@@ -1,8 +1,9 @@
 package com.rhcloud.tigerden.apollo;
 
-import com.rhcloud.tigerden.apollo.drawing.GameBoard;
+import java.util.Random;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
@@ -10,9 +11,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.rhcloud.tigerden.apollo.drawing.GameBoard;
+
 public class MainActivity extends Activity implements OnClickListener {
 	
 	private Handler frame = new Handler();
+	
+	private Point sprite1Velocity;
+	private Point sprite2Velocity;
+	private int sprite1MaxX;
+	private int sprite1MaxY;
+	private int sprite2MaxX;
+	private int sprite2MaxY;
 	
 	private static final int FRAME_RATE = 20;
 
@@ -34,9 +44,53 @@ public class MainActivity extends Activity implements OnClickListener {
 
 	synchronized public void initGfx() {
 		((GameBoard)findViewById(R.id.canvas)).resetStarField();
+		
+		Point p1,p2;
+		
+		do{
+			p1 = getRandomPoint();
+			p2 = getRandomPoint();
+		} while (Math.abs(p1.x - p2.x) < ((GameBoard)findViewById(R.id.canvas)).getSprite1Width());
+		
+		
+		((GameBoard)findViewById(R.id.canvas)).setSprite1(p1.x, p1.y);
+		((GameBoard)findViewById(R.id.canvas)).setSprite2(p2.x, p2.y);
+		
+		sprite1Velocity = getRandomVelocity();
+		sprite2Velocity = new Point(1,1);
+		
+		sprite1MaxX = ((GameBoard)findViewById(R.id.canvas)).getWidth() - ((GameBoard)findViewById(R.id.canvas)).getSprite1Width();
+		sprite1MaxY = ((GameBoard)findViewById(R.id.canvas)).getHeight() - ((GameBoard)findViewById(R.id.canvas)).getSprite1Height();
+		sprite2MaxX = ((GameBoard)findViewById(R.id.canvas)).getWidth() - ((GameBoard)findViewById(R.id.canvas)).getSprite2Width();
+		sprite2MaxY = ((GameBoard)findViewById(R.id.canvas)).getWidth() - ((GameBoard)findViewById(R.id.canvas)).getSprite2Height();
+		
 		((Button)findViewById(R.id.main_button)).setEnabled(true);
 		frame.removeCallbacks(frameUpdate);
 		frame.postDelayed(frameUpdate, FRAME_RATE);
+	}
+
+	private Point getRandomPoint() {
+		Random r = new Random();
+		int minX = 0;
+		int maxX = findViewById(R.id.canvas).getWidth() - ((GameBoard)findViewById(R.id.canvas)).getSprite1Width();
+		int x = 0;
+		int minY = 0;
+		int maxY = findViewById(R.id.canvas).getHeight() - ((GameBoard)findViewById(R.id.canvas)).getSprite1Height();
+		int y = 0;
+		
+		x = r.nextInt(maxX - minX + 1) + minX;
+		y = r.nextInt(maxY - minY + 1) + minY;
+
+		return new Point(x,y);
+	}
+	
+	private Point getRandomVelocity(){
+		Random r = new Random();
+		int min = 1;
+		int max = 5;
+		int x = r.nextInt(max-min+1) + min;
+		int y = r.nextInt(max-min+1) + min;
+		return new Point(x,y);
 	}
 
 	@Override
@@ -54,8 +108,30 @@ public class MainActivity extends Activity implements OnClickListener {
 	private Runnable frameUpdate = new Runnable() {
 
 		@Override
-		public void run() {
+		synchronized public void run() {
 			frame.removeCallbacks(frameUpdate);
+			Point sprite1 = new Point(((GameBoard)findViewById(R.id.canvas)).getSprite1X(), ((GameBoard)findViewById(R.id.canvas)).getSprite1Y());
+			Point sprite2 = new Point(((GameBoard)findViewById(R.id.canvas)).getSprite2X(), ((GameBoard)findViewById(R.id.canvas)).getSprite2Y());
+			sprite1.x = sprite1.x + sprite1Velocity.x;
+			if(sprite1.x > sprite1MaxX || sprite1.x < 5){
+				sprite1Velocity.x *= -1;
+			}
+			sprite1.y = sprite1.y + sprite1Velocity.y;
+			if(sprite1.y > sprite1MaxY || sprite1.y < 5){
+				sprite1Velocity.y *= -1;
+			}
+			sprite2.x = sprite2.x + sprite2Velocity.x;
+			if(sprite2.x > sprite2MaxX || sprite2.x < 5){
+				sprite2Velocity.x *= -1;
+			}
+			sprite2.y = sprite2.y + sprite2Velocity.y;
+			if(sprite2.y > sprite2MaxY || sprite2.y < 5){
+				sprite2Velocity.y *= -1;
+			}
+			
+			((GameBoard)findViewById(R.id.canvas)).setSprite1(sprite1.x, sprite1.y);
+			((GameBoard)findViewById(R.id.canvas)).setSprite2(sprite2.x, sprite2.y);
+			
 			((GameBoard)findViewById(R.id.canvas)).invalidate();
 			frame.postDelayed(frameUpdate, FRAME_RATE);
 		}
--- Apollo/src/com/rhcloud/tigerden/apollo/drawing/GameBoard.java
+++ Apollo/src/com/rhcloud/tigerden/apollo/drawing/GameBoard.java
@@ -5,12 +5,18 @@ import java.util.List;
 import java.util.Random;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.rhcloud.tigerden.apollo.R;
 
 public class GameBoard extends View {
 
@@ -18,6 +24,20 @@ public class GameBoard extends View {
 	private List<Point> starField = null;
 	private int starAlpha = 80;
 	private int starFade = 2;
+	
+	// Sprite vars
+	private Rect sprite1Bounds = new Rect(0,0,0,0);
+    private Rect sprite2Bounds = new Rect(0,0,0,0);
+    private Point sprite1;
+    private Point sprite2;
+    
+    private Bitmap bm1 = null;
+    private Bitmap bm2 = null;
+    
+    private Matrix m = null;
+    
+    private float spriteRotation = 0;
+	
 	private static final int NUM_OF_STARS = 25;
 	synchronized public void resetStarField(){
 		starField = null;
@@ -26,8 +46,65 @@ public class GameBoard extends View {
 	
 	public GameBoard(Context context, AttributeSet aSet) {
 		super(context, aSet);
+		m = new Matrix();
 		p = new Paint();
+		sprite1 = new Point(-1,-1);
+		sprite2 = new Point(-1,-1);
+		bm1 = BitmapFactory.decodeResource(getResources(),R.drawable.asteroid);
+		bm2 = BitmapFactory.decodeResource(getResources(), R.drawable.ufo);
+		
+		sprite1Bounds = new Rect(0,0,bm1.getWidth(),bm1.getHeight());
+		sprite2Bounds = new Rect(0,0,bm2.getWidth(),bm2.getHeight());
 	}
+	
+	synchronized public void setSprite1(int x, int y){
+		sprite1 = new Point (x, y);
+	}
+	
+	synchronized public void setSprite2(int x, int y){
+		sprite2 = new Point(x, y);
+	}
+	
+	synchronized public Point getSprite1(){
+		return sprite1;
+	}
+	
+	synchronized public int getSprite1X(){
+		return sprite1.x;
+	}
+	
+	synchronized public int getSprite1Y(){
+		return sprite1.y;
+	}
+	
+	synchronized public int getSprite1Width(){
+		return sprite1Bounds.width();
+	}
+	
+	synchronized public int getSprite1Height(){
+		return sprite1Bounds.height();
+	}
+	
+	synchronized public Point getSprite2(){
+		return sprite2;
+	}
+	
+	synchronized public int getSprite2X(){
+		return sprite2.x;
+	}
+	
+	synchronized public int getSprite2Y(){
+		return sprite2.y;
+	}
+	
+	synchronized public int getSprite2Width(){
+		return sprite2Bounds.width();
+	}
+	
+	synchronized public int getSprite2Height(){
+		return sprite2Bounds.height();
+	}
+	
 	
 	private void initializeStars(int maxX, int maxY) {
 		starField = new ArrayList<Point>();
@@ -56,6 +133,20 @@ public class GameBoard extends View {
 		p.setStrokeWidth(5);
 		for(int i=0; i < NUM_OF_STARS; i++){
 			canvas.drawPoint(starField.get(i).x, starField.get(i).y, p);
+		}
+		
+		if(sprite1.x >= 0){
+			m.reset();
+			m.postTranslate((float) sprite1.x, (float) sprite1.y);
+			m.postRotate(spriteRotation, (float) (sprite1.x + sprite1Bounds.width() / 2.0), (float) (sprite1.y + sprite1Bounds.height() / 2.0));
+			canvas.drawBitmap(bm1, m, null);
+			spriteRotation += 5;
+			if(spriteRotation >= 360)
+				spriteRotation = 0;
+		}
+		
+		if(sprite2.x >= 0){
+			canvas.drawBitmap(bm2, sprite2.x, sprite2.y, null);
 		}
 	}
 
